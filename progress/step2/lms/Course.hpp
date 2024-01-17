d92f25dbd4e1c8ae1c897fc53fb9168020b3215e
Saif eddine El hanoune <sel-hano@e1r9p1.1337.ma>
2024-01-17 02:00:26 +0100

    [WIFIXEDIT] turn back to ex00

---
+++ progress/step2/lms/Course.hpp
@@ -0,0 +1,23 @@
+#ifndef COURSE_H
+#define COURSE_H
+
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class Course {
+    private:
+        std::string CourseName;
+        int CourseID;
+    public:
+        Course(const std::string& name, int id) : CourseName(name), CourseID(id) {}
+        std::string GetCourseName() const {
+            return CourseName;
+        }
+        int GetCourseID() const {
+            return CourseID;
+        }
+};
+
+#endif
---
+++ progress/step2/lms/Students.hpp
@@ -0,0 +1,12 @@
+#ifndef LMS_H
+#define LMS_H
+#include "Course.hpp"
+
+class Student {
+    private:
+        char *name;
+        int id;
+        Course courses[10];     
+}
+
+#endif
