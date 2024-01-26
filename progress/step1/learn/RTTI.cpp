bf665072cc811779e6d6d58ab4870921c9e298c8
Saif eddine El hanoune <sel-hano@e1r6p7.1337.ma>
2024-01-26 16:00:08 +0100

    status changed
    Signed-off-by: Saif eddine El hanoune <sel-hano@e1r6p7.1337.ma>

--- progress/step1/learn/RTTI.cpp
+++ progress/step1/learn/RTTI.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <typeinfo>
 
+/*
+    Run-Time Type Identification (RTTI) is a feature in C++ that allows you to obtain the type information of an object during program execution. 
+    This can be useful when using dynamic typing, where the type of an object can change at runtime.
+    There are two main mechanisms for RTTI in C++:
+    typeid operator
+    dynamic_cast operator
+    typeid operator
+    typeid is an operator that returns a reference to an object of type std::type_info, 
+    which contains information about the type of the object. 
+    The header file <typeinfo> should be included to use typeid.
+*/
+
 class Base { virtual void dummy() {} };
 class Derived : public Base { /* ... */ };
 
