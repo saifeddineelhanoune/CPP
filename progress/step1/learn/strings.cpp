6b50ada4487aff3151ead4f2abdd1fc16559def3
Saif eddine El hanoune <sel-hano@e1r6p7.1337.ma>
2024-01-26 15:51:16 +0100

    strings

---
+++ progress/step1/learn/strings.cpp
@@ -0,0 +1,18 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+// Replace operator new and delete to log allocations
+void* operator new(std::size_t n) {
+    std::cout << "[Allocating " << n << " bytes]";
+    return malloc(n);
+}
+void operator delete(void* p) throw() {
+    free(p);
+}
+
+int main() {
+    for (size_t i = 0; i < 24; ++i) {
+        std::cout << i << ": " << std::string(i, '=') << std::endl;
+    }
+}
