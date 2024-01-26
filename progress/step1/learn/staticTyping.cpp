07e39e5675f71833e84a8b21505da6af7c69b202
Saif eddine El hanoune <sel-hano@e1r6p7.1337.ma>
2024-01-26 16:06:43 +0100

    [LEARNING] static typing
    Signed-off-by: Saif eddine El hanoune <sel-hano@e1r6p7.1337.ma>

---
+++ progress/step1/learn/staticTyping.cpp
@@ -0,0 +1,18 @@
+/*
+
+    Static Typing
+
+    n C++, static typing means that the data type of a variable is determined at compile time, 
+    before the program is executed. 
+    This means that a variable can be used only with data of a specific type, 
+    and the compiler ensures that the operations performed with the variable are 
+    compatible with its type.
+
+    C++ is a statically typed language, 
+    which means that it uses static typing 
+    to determine data types and perform type checking during compile time.
+    This helps with ensuring type safety and can prevent certain types of errors 
+    from occurring during the execution of the program.
+
+*/
+
