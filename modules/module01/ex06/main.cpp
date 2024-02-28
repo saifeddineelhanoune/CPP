9723bccbbe33c0f70b9decaf06cec897e4a251e8
saif <saifeddine13elhanoune@gmail.com>
2024-02-28 02:45:16 +0100

    module 01 is done

--- modules/module01/ex06/main.cpp
+++ modules/module01/ex06/main.cpp
@@ -3,7 +3,7 @@
 int main(int ac, char *av[]) {
     Harl *harl = new Harl();
     if (ac == 2)
-        return harl->complain(av[1]), 0;
+        return harl->complain(av[1]), delete harl, 0;
     else
-        return std::cout << "level not found" << std::endl, 1;
+        return std::cout << "level not found" << std::endl, delete harl, 1;
 }
