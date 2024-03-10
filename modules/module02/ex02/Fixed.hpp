fb6e33eb5184bf8657cb5ad3b3f0f61f4f381b48
saifeddine elhanoune <saifeddine13elhanoune@gmail.com>
2024-03-10 02:51:07 +0000

    ex02 class Fixed.hpp is modified successfuly

---
+++ modules/module02/ex02/Fixed.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+
+class Fixed
+{
+    private:
+        int	fixed;
+        static int const fractional = 8;
+
+    public:
+        Fixed();
+        Fixed(const Fixed &f);
+        Fixed(int const nb);
+        Fixed(float const nb);
+        ~Fixed();
+        Fixed   &operator=(const Fixed &fixed);
+        int     getRawBits(void) const;
+        void    setRawBits(int const raw);
+        int     toInt(void) const;
+        float   toFloat(void) const;
+        bool	operator>(Fixed const & raw) const;
+        bool	operator<(Fixed const & raw) const;
+        bool	operator>=(Fixed const & raw) const;
+        bool	operator<=(Fixed const & raw) const;
+        bool	operator==(Fixed const & raw) const;
+        bool	operator!=(Fixed const & raw) const;
+        Fixed	operator+(Fixed const & raw) const;
+        Fixed	operator-(Fixed const & raw) const;
+        Fixed	operator*(Fixed const & raw) const;
+        Fixed	operator/(Fixed const & raw) const;
+        Fixed	operator++(int);
+        Fixed	operator--(int);
+        Fixed   &operator++(void);
+        Fixed   &operator--(void);
+        static Fixed &min(Fixed &f1, Fixed &f2);
+        static Fixed &max(Fixed &f1, Fixed &f2);
+        static Fixed const &min(Fixed const &f1, Fixed const &f2);
+        static Fixed const &max(Fixed const &f1, Fixed const &f2);
+};
+
+std::ostream &	operator<<(std::ostream &o, Fixed const &tmp);
