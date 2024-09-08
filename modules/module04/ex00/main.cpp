044264a165e9d62810815bca4f32d01b4452ee4c
Saif eddine El hanoune <saifeddine13elhanoune@gmail.com>
2024-09-09 00:04:18 +0100

    fixing wrong tests

--- modules/module04/ex00/main.cpp
+++ modules/module04/ex00/main.cpp
@@ -1,19 +1,35 @@
-#include "Cat.hpp"
+#include "Cat.hpp" 
 #include "Dog.hpp"
+#include "WrongCat.hpp"
 
 int main()
 {
     const Animal* meta = new Animal();
     const Animal* j = new Dog();
     const Animal* i = new Cat();
+
+    const WrongAnimal *wrong = new WrongAnimal();
+    const WrongAnimal* wrongcat = new WrongCat();
+
     std::cout << meta->getType() << " " << std::endl;
     std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
+    
+    std::cout << wrong->getType() << " " << std::endl;
+    std::cout << wrongcat->getType() << " " << std::endl;
+    
     i->makeSound();
     j->makeSound();
     meta->makeSound();
+
+    wrong->makeSound();
+    wrongcat->makeSound();
+
     delete j;
     delete i;
     delete meta;
+
+    delete wrong;
+    delete wrongcat;
     return 0;
 }
